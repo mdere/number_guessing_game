12330796428e92de717ba529bf82d6f90d63ea65
Michael Dere <michael.dere@opower.com>
2014-10-14 00:39:39 +0000

    Updating script 1

--- random_num_project.rb
+++ random_num_project.rb
@@ -16,29 +16,62 @@
 # 3. Give rescue/begin case
 # 4. Ask user to play again. → yes “Horray!” or no “Aw, shucks…”
 
-puts "Do you want override total guesses in this game?"
+puts "Do you want override total guesses in this game? (Y/N)"
 prompt_guess = $stdin.gets.chomp.downcase
-if prompt_guess == "y" || prompt_guess == "yes"
-  puts "How many guesses do you want?"
-  total_guesses = $stdin.gets.chomp
-  puts "You chose #{total_guesses} guesses!"
-else
-  total_guesses = 5
-  puts "defaulted guesses is #{total_guesses}"
+waiting = true
+while waiting
+  if prompt_guess == "y" || prompt_guess == "yes"
+    puts "How many guesses do you want?"
+    total_guesses = $stdin.gets.chomp
+    puts "You chose #{total_guesses} guesses!"
+    waiting = false
+  elsif prompt_guess == "n" || prompt_guess == "no"
+    total_guesses = 5
+    puts "defaulted guesses is #{total_guesses}"
+    waiting = false
+  else 
+    puts "Incorrect input"
+  end
 end
 
-puts "Lower threshold?"
-min = $stdin.gets.chomp
-puts "You set lower threshold to be #{min}"
+puts "Do you want override lower threshold in this game?"
+prompt_guess = $stdin.gets.chomp.downcase
+waiting = true
+while waiting
+  if prompt_guess == "y" || prompt_guess == "yes"
+    puts "Lower threshold?"
+    min = $stdin.gets.chomp
+    puts "You set lower threshold to be #{min}"
+  elsif prompt_guess == "n" || prompt_guess == "no"
+    min = 0
+    puts "defaulted lower threshold is #{min}"
+    waiting = false
+  else 
+    puts "Incorrect input"
+  end
+end    
 
+puts "Do you want override upper threshold in this game?"
+prompt_guess = $stdin.gets.chomp.downcase
 max_is_set = false
-while !max_is_set
-  puts "Upper threshold?"
-  max = $stdin.gets.chomp  
-  if max <= min
-    puts "You may not do that, choose a number higher than #{min}" 
+waiting = true
+while waiting
+  if prompt_guess == "y" || prompt_guess == "yes"  
+    while !max_is_set
+      puts "Upper threshold?"
+      max = $stdin.gets.chomp  
+      if max <= min
+        puts "You may not do that, choose a number higher than #{min}" 
+      else
+        puts "You set upper threshold to be #{max}"
+        max_is_set = true
+      end
+    end
+  elsif prompt_guess == "n" || prompt_guess == "no"
+    max = 10
+    puts "defaulted upper threshold is #{max}"
+    waiting = false
   else
-    puts "You set upper threshold to be #{max}"
-    max_is_set = true
+    puts "Incorrect input"    
   end
 end
